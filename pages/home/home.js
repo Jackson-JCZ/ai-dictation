d4de5da94fb5b81c915b1524b82909912329d39c
Jkey104 <1043836038@qq.com>
2021-05-02 01:16:19 +0800

    添加客服按钮

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,32 +1,32 @@
 // pages/home/home.js
 Page({
-  data:{
+  data: {
     curPage: "home",
     curBar: '1',
   },
   navChange(e) {
-		this.setData({
-			curPage: e.currentTarget.dataset.cur
-		})
+    this.setData({
+      curPage: e.currentTarget.dataset.cur
+    })
   },
-  click1(){
+  click1() {
     wx.navigateTo({
       url: '/pages/photo/photo',
     })
   },
-  click2(){
+  click2() {
     wx.navigateTo({
       url: '/pages/select/select',
     })
   },
-  click3(){
+  click3() {
     wx.navigateTo({
       url: '/pages/challenge/challenge',
     })
   },
   barChange(e) {
-		this.setData({
-			curBar: e.currentTarget.dataset.cur
-		})
+    this.setData({
+      curBar: e.currentTarget.dataset.cur
+    })
   },
 })
