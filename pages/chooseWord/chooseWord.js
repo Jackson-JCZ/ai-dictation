ba955902ecfd08361d7dce3410443b6e12a826f1
Jkey104 <1043836038@qq.com>
2021-05-05 16:51:39 +0800

    show words

--- pages/chooseWord/chooseWord.js
+++ pages/chooseWord/chooseWord.js
@@ -1,4 +1,5 @@
 // pages/chooseWord/chooseWord.js
+const app = getApp();
 Page({
 
   /**
@@ -12,7 +13,9 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      wordsList: app.globalData.wordsList
+    })
   },
 
   /**
