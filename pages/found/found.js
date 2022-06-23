292bae60c2106fecaddbd652428c11a70a22459c
Donald_ga <313771069@qq.com>
2022-06-23 09:48:37 +0800

    完善发现页面榜单

--- pages/found/found.js
+++ pages/found/found.js
@@ -2,7 +2,35 @@ Page({
   data: {
     TabCur: 0,
     scrollLeft:0,
-    TabIndex:['排行榜','记笔顺','语法站']
+    TabIndex:['排行榜','记笔顺','语法站'],
+    lastdayIncome: [
+      {
+        name: '小明',
+        headPhone: '../../images/头像1.png',
+        total: 524
+      },
+      {
+        name: '小往',
+        headPhone: '../../images/头像2.png',
+        total: 499
+      },
+      {
+        name: '小葱',
+        headPhone: '../../images/头像3.png',
+        total: 399
+      },
+      {
+        name: 'doit',
+        headPhone: '../../images/头像1.png',
+        total: 200
+      },
+      {
+        name: 'jkey',
+        headPhone: '../../images/头像1.png',
+        total: 100
+      }
+      
+    ]
   },
   tabSelect(e) {
     this.setData({
