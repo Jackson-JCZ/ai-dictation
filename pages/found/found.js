fda7932329b0c437c8a65ee9bec0b6315c4156aa
Jkey104 <1043836038@qq.com>
2022-06-23 14:32:40 +0800

    Merge changes

--- pages/found/found.js
+++ pages/found/found.js
@@ -1,8 +1,36 @@
 Component({
   data: {
     TabCur: 0,
-    scrollLeft: 0,
-    TabIndex: ['排行榜', '记笔顺', '语法站']
+    scrollLeft:0,
+    TabIndex:['排行榜','记笔顺','语法站'],
+    lastdayIncome: [
+      {
+        name: '小明',
+        headPhone: '../../images/头像1.png',
+        total: 524
+      },
+      {
+        name: '小往',
+        headPhone: '../../images/头像2.png',
+        total: 499
+      },
+      {
+        name: '小葱',
+        headPhone: '../../images/头像3.png',
+        total: 399
+      },
+      {
+        name: 'doit',
+        headPhone: '../../images/头像1.png',
+        total: 200
+      },
+      {
+        name: 'jkey',
+        headPhone: '../../images/头像1.png',
+        total: 100
+      }
+      
+    ]
   },
   methods: {
     tabSelect(e) {
