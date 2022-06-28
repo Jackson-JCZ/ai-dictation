d03daad7f463f18c7776bf8e96e85561fd1a4a50
Jkey104 <1043836038@qq.com>
2022-06-29 00:06:06 +0800

    增加学习完成界面

--- pages/chooseWord/dictation/complete/complete.js
+++ pages/chooseWord/dictation/complete/complete.js
@@ -5,14 +5,17 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    wordsList : []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    const app = getApp();
+    this.setData({
+      wordsList: app.globalData.wordsList
+    });
   },
 
   /**
@@ -23,4 +26,18 @@ Page({
       delta: 2
     })
   },
+
+  /**
+   * 生命周期函数--监听用户分享
+   */
+  onShareAppMessage(){
+    return {
+      title: `我在小学英语学习小程序学习了${this.data.wordsList.length}个单词，你也来看看吧!`,
+      path: '/pages/home/home'
+    }
+  },
+
+  back() {
+    wx.navigateBack()
+  }
 })
